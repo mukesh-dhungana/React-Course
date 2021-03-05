494ff4e56d11e4e260e2bb8c418c7d4d63d37bbb
FIRST_NAME LAST_NAME <rujal.sapkota@aratitech.org>
2021-03-05 12:41:53 +0545

    Changed css work

--- React Task/Rujal/fourth-task/src/Components/Card.js
+++ React Task/Rujal/fourth-task/src/Components/Card.js
@@ -7,7 +7,7 @@ function Card({ id, title, body, onClick }) {
             <div className="card-header">
                 <h2>{id}. {FirstLetterCapital(title)}</h2>
             </div>
-            <div style={{ borderBottom: '1px solid rgba(255,255,255,.5)', width: '90%', margin: 'auto' }}></div>
+            <div style={{ borderBottom: '1px solid #1289A7', width: '90%', margin: 'auto' }}></div>
             <div className="card-content">
                 {FirstLetterCapital(body)}
             </div>
