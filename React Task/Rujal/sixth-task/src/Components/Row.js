44b00df283477729dcd7ed263f1f00166d82a213
FIRST_NAME LAST_NAME <rujal.sapkota@aratitech.org>
2021-03-09 14:35:32 +0545

    same

--- React Task/Rujal/sixth-task/src/Components/Row.js
+++ React Task/Rujal/sixth-task/src/Components/Row.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react'
 
 export default class Row extends Component {
     render() {
+        const { id, name, address } = this.props
         return (
             <tr>
-                <td>{this.props.id}</td>
-                <td>{this.props.name}</td>
-                <td>{this.props.address}</td>
+                <td>{id}</td>
+                <td>{name}</td>
+                <td>{address}</td>
                 <td>
                     <button>Delete</button>
                 </td>
