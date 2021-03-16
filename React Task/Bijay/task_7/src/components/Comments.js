d93ffa1ce5fdaacfdaded2e27fad821a854c4962
beezay bohora <bijay.okur@gmail.com>
2021-03-16 12:25:12 +0545

    modal added

--- React Task/Bijay/task_7/src/components/Comments.js
+++ React Task/Bijay/task_7/src/components/Comments.js
@@ -1,30 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
+import Modal from "./Modal";
 import "./Styles/Comments.css";
 
-const Comments = ({comment, deleteComment}) => {
+const Comments = ({ comment, deleteComment }) => {
+  // console.log('Comment Section', comment);
+  const showModal = () => {
+    console.log("Modal Shown");
+    setModal(true);
+  };
 
-    // console.log('Comment Section', comment);
+  const toggleModal = () => {
+    console.log("Clicked");
+    setModal(false);
+  };
+
+  const [modal, setModal] = useState(false);
 
   return (
-    <div className="comments">
-      <div className="comment-title">
-        <p>{comment.comment}</p>
-      </div>
-      <div className="comment-details">
-        <div className="comment">
-          <p>
-            <i className="fas fa-user"></i> {comment.comment_by}
-          </p>
-          <p>
-            <i className="fas fa-calendar-alt"></i> {comment.commented_at}
-          </p>
+    <>
+      {modal && (
+        <Modal
+          toggleModal={toggleModal}
+          deleteComment={deleteComment}
+          comment={comment}
+        />
+      )}
+      <div className="comments">
+        <div className="comment-title">
+          <p>{comment.comment}</p>
         </div>
-        <div className="comment-action">
-            <p ><i className="fas fa-user-edit edit"></i></p>
-            <p ><i onClick={() => deleteComment(comment.id)} className="fas fa-trash-alt delete"></i></p>
+        <div className="comment-details">
+          <div className="comment">
+            <p>
+              <i className="fas fa-user"></i> {comment.comment_by}
+            </p>
+            <p>
+              <i className="fas fa-calendar-alt"></i> {comment.commented_at}
+            </p>
+          </div>
+          <div className="comment-action">
+            <p>
+              <i
+                onClick={() => showModal()}
+                className="fas fa-user-edit edit"
+              ></i>
+            </p>
+            <p>
+              <i
+                onClick={() => {
+                  deleteComment(comment.id);
+                }}
+                className="fas fa-trash-alt delete"
+              ></i>
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- React Task/Bijay/task_7/src/components/Post.js
+++ React Task/Bijay/task_7/src/components/Post.js
@@ -1,45 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import Comments from "./Comments";
+import Modal from "./Modal";
 import "./Styles/Post.css";
 
-const Post = ({post, dispatch}) => {
-    const comments = post.comments
+const Post = ({ post, dispatch }) => {
+  const comments = post.comments;
 
-    const deleteComment = (id) => {
-        console.log('Deleted', id);
-        dispatch({
-            type: 'DELETE_COMMENT',
-            payload: {
-                postId: post.user_id,
-                commentId: id
-            }
-        })
-    }
-    
-    console.log('Comments', comments);
+  const deleteComment = id => {
+    console.log("Deleted", id);
+    dispatch({
+      type: "DELETE_COMMENT",
+      payload: {
+        postId: post.user_id,
+        commentId: id,
+      },
+    });
+  };
+
+  const addComment = () => {
+    setModal(true);
+  };
+
+  const toggleModal = () => {
+    console.log("Clicked");
+    setModal(false);
+  };
+
+  const [modal, setModal] = useState(false);
+
+  console.log("Comments", comments);
   return (
-    <div className="post">
-      <div className="post-header">
-        <div className="post-heading">
-          <h4>{post.title}</h4>
+    <>
+      {modal && <Modal toggleModal={toggleModal}/>}
+      <div className="post">
+        <div className="post-header">
+          <div className="post-heading">
+            <h4>{post.title}</h4>
+          </div>
+          <div className="post-details">
+            <p className="post-user">
+              <i className="fas fa-user"></i>
+              {post.user_name}
+            </p>
+            <p className="post-date">
+              <i className="fas fa-calendar-alt"></i> {post.created_at}
+            </p>
+            <p className="add-comment">
+              <i onClick={addComment} class="fas fa-plus-circle"></i>
+            </p>
+          </div>
         </div>
-        <div className="post-details">
-          <p className="post-user">
-            <i className="fas fa-user"></i>{post.user_name}
-          </p>
-          <p className="post-date">
-            <i className="fas fa-calendar-alt"></i> {post.created_at}
-          </p>
-        </div>
-      </div>
-      <hr />
-      <div className="comments-section">
-        {/* <Comments comments={comments}/> */}
-        {comments.map((comment) => (
+        <hr />
+        <div className="comments-section">
+          {/* <Comments comments={comments}/> */}
+          {comments.map(comment => (
             <Comments comment={comment} deleteComment={deleteComment} />
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
