92f3b28738137e20f98cf5537387b59e128a5752
FIRST_NAME LAST_NAME <rujal.sapkota@aratitech.org>
2021-03-17 13:59:52 +0545

    added redirect home

--- React Task/Rujal/tenth-task/src/App.js
+++ React Task/Rujal/tenth-task/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom'
 import Home from './Components/Home'
 import About from './Components/About'
 import Posts from './Components/Posts'
@@ -15,6 +15,7 @@ function App() {
           <Route path="/about" component={About} />
           <Route path="/posts" component={Posts} />
           <Route path="/post/:id" component={Post} />
+          <Redirect from="/home" to="/" />
         </Switch>
       </Router>
     </div>
