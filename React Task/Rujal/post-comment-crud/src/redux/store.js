83bad8a9b6ed9fe606577b754f894b0837ff6ddc
FIRST_NAME LAST_NAME <rujal.sapkota@aratitech.org>
2021-03-25 16:51:58 +0545

    some modification on store

--- React Task/Rujal/post-comment-crud/src/redux/store.js
+++ React Task/Rujal/post-comment-crud/src/redux/store.js
@@ -2,6 +2,6 @@ import { applyMiddleware, createStore, compose } from 'redux'
 import reducer from './reducer'
 
 const middleware = store => next => action => typeof action === "function" ? action(store.dispatch) : next(action)
-const enhanceCompose = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__|| compose
+const enhanceCompose = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 export const store = createStore(reducer, enhanceCompose(applyMiddleware(middleware)))
