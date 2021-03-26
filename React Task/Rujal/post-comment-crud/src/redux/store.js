ad4273ed5ab8da7d9f914f46f590a7101ba75131
FIRST_NAME LAST_NAME <rujal.sapkota@aratitech.org>
2021-03-26 14:27:00 +0545

    Added persist state in redux

--- React Task/Rujal/post-comment-crud/src/redux/store.js
+++ React Task/Rujal/post-comment-crud/src/redux/store.js
@@ -1,7 +1,31 @@
 import { applyMiddleware, createStore, compose } from 'redux'
 import reducer from './reducer'
 
+const saveState = (state) => {
+    try {
+        const serializedState = JSON.stringify(state);
+        localStorage.setItem('state', serializedState);
+    } catch {
+        // ignore write errors
+    }
+};
+const loadState = () => {
+    try {
+        const serializedState = localStorage.getItem('state');
+        if (serializedState === null) {
+            return undefined;
+        }
+        return JSON.parse(serializedState);
+    } catch (err) {
+        return undefined;
+    }
+};
+
+const persistedState = loadState();
+
 const middleware = store => next => action => typeof action === "function" ? action(store.dispatch) : next(action)
 const enhanceCompose = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
-export const store = createStore(reducer, enhanceCompose(applyMiddleware(middleware)))
+export const store = createStore(reducer, persistedState, enhanceCompose(applyMiddleware(middleware)))
+
+store.subscribe(() => saveState(store.getState()))
