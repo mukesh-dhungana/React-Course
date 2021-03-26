4f6dace127be5bcaa7e4d63c2d6f4f867d934c4b
Ashok <ashok.ag.ganika@gmail.com>
2021-03-26 16:03:31 +0545

    store persisted

--- React Task/Ashok/redux-crud/src/redux/store.js
+++ React Task/Ashok/redux-crud/src/redux/store.js
@@ -8,8 +8,34 @@ const asynActionMiddleware = (store) => (next) => (action) => {
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+export const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem("state");
+    if (serializedState === null) {
+      return undefined;
+    }
+    return JSON.parse(serializedState);
+  } catch (err) {
+    return undefined;
+  }
+};
+const persistedState = loadState();
+
 export const store = createStore(
   reducer,
-
+  persistedState,
   composeEnhancers(applyMiddleware(asynActionMiddleware))
 );
+
+export const saveState = (state) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem("state", serializedState);
+  } catch {
+    console.log("error saving state");
+  }
+};
+
+store.subscribe(() => {
+  saveState(store.getState());
+});
